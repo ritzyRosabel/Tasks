d1160bf4cf3de53e474314d8ced9bfd5a683e9b6
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-12 22:45:21 +0100

    things to do with task

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -11,6 +11,11 @@ namespace Tasks
         static void Main(string[] args)
         {
             // Task is a basic unit of the Task parrallel library
+            //can perform the following  with a task
+            // start/run
+            //wait
+            //cancel
+            //have a child task
             Task Parent = new Task(() =>
             {
                 Task Child = new Task(() =>
