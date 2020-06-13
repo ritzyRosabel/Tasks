b0fcb9073e793f21bc99e85ef63faf01c56b07e0
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-13 14:45:15 +0100

    adding task to another class.

---
+++ Tasks/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Tests
+    {
+        // task property delay 
+        Task t = new Task(() =>
+        {
+            Task.Delay(2000);
+
+        });
+    }
+}
